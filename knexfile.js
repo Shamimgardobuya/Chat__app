1958e1c8d8245fad30ddcf1fa277b8a73d457ce4
Shamimgardobuya <shamimobuya@gmail.com>
2025-04-17 09:11:19 +0300

    corrected db connection

--- knexfile.js
+++ knexfile.js
@@ -36,12 +36,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
