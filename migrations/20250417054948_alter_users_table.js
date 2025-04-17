5e3dac826299e11c8352317fbb3aa5129cfa2f7e
Shamimgardobuya <shamimobuya@gmail.com>
2025-04-17 08:52:19 +0300

    added migration file

---
+++ migrations/20250417054948_alter_users_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable('users', (table)=> {
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+    })
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+
+};
